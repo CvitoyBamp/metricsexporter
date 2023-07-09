61d5d8c85fdbefa7e3b433abaa32d901e2453015
CvitoyBamp <cvitoybamp@yandex.ru>
2023-07-09 19:06:21 +0300

    iter4 v2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,13 +3,35 @@ package main
 import (
 	"flag"
 	"github.com/CvitoyBamp/metricsexporter/internal/agent"
+	"log"
+	"strconv"
 )
 
+var (
+	addressS,
+	reportIntervalS,
+	pollIntervalS *string
+)
+
+func init() {
+	addressS = flag.String("a", "http://localhost:8080", "An address the server will send metrics")
+	reportIntervalS = flag.String("r", "10", "An interval for sending metrics to server")
+	pollIntervalS = flag.String("p", "2", "An interval for collecting metrics")
+}
+
 func main() {
-	address := flag.String("a", "localhost:8080", "An address the server will send metrics")
-	reportInterval := flag.Int("r", 10, "An interval for sending metrics to server")
-	pollInterval := flag.Int("p", 2, "An interval for collecting metrics")
 	flag.Parse()
-	c := agent.CreateAgent(*address)
-	c.RunAgent(*pollInterval, *reportInterval)
+
+	pollInterval, err := strconv.Atoi(*pollIntervalS)
+	if err != nil {
+		log.Fatalf("can't parse pollInterval. Err: %s", err)
+	}
+
+	reportInterval, err := strconv.Atoi(*reportIntervalS)
+	if err != nil {
+		log.Fatalf("can't parse reportInterval. Err: %s", err)
+	}
+
+	c := agent.CreateAgent(*addressS)
+	c.RunAgent(pollInterval, reportInterval)
 }
